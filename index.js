90fcd622ddcee066f2f58de99b45cdf2986b2132
tombena <tbenacin@gmail.com>
2017-02-26 18:54:29 +0100

    Final commit

--- index.js
+++ index.js
@@ -13,27 +13,31 @@ function get_date_formatted() {
 
 var src = './files/original/'
 var dest = './files/moved/'
-var arr, name, extension
 
 // create destination folder if it doesn't exist yet
 if (!fs.existsSync(dest)) {
     fs.mkdir(dest, (err) => {
-		if (err) {
+		if (err)
 			return console.log(`Unable to create ${dest} directory`);
-		}
 	})
 }
 
+var file_count = 0;
+var file_remaining = 0;
+var err_count = 0;
+var err_files = []
+var arr, name, extension
+
 // list contents of folder
 fs.readdir(src, (err, files) => {
-	files.forEach(file => {
+	if(err)
+		return console.log(`Unable to read ${dest} directory`);
 
+	files.forEach(file => {
 		arr = file.split(".");
 		extension = arr[arr.length - 1]
 		//file name without extension
 		name = arr[0]
-
-		// pick files and not directories or hidden files????
 
 		// if multiple extensions file name is file without last extension
 		if (arr.length > 2) {
@@ -43,11 +47,14 @@ fs.readdir(src, (err, files) => {
 		}
 
 		if (extension == 'txt' || extension == 'json') {
+			file_count++;
+			file_remaining++;
+
 			var date = get_date_formatted()
 			const new_name = name + "_EDITED_" + date + "." + extension
 			const data = [file, src.substr(2) + file, new_name, dest.substr(2) + new_name]
 
-			// append and rename should happen sequentially
+			// append_file_info and rename should happen sequentially
 			async.series([
 				function(callback) {
 			    	append.append_file_info(src + file, data, extension, callback)
@@ -57,17 +64,18 @@ fs.readdir(src, (err, files) => {
 				}
 			], function(err) {
 				if (err) {
-		    		return console.log(err);
+					err_count++
+					err_files.push(file)	
 				}
-			})
-		}
+				file_remaining--
 
-		if (err) {
-		    return console.log(err);
+				// print output when all files have been treated
+				if (file_remaining == 0)
+					if (err_count == 0)
+						console.log(`Renamed ${file_count} files, with 0 errors`)
+					else
+						console.log(`Renamed ${file_count - err_count} files, with ${err_count} error(s) (${err_files})`)
+			})
 		}
 	})
 })
-
-// CHECK FOR ERRORS
-// check there are no .txt nor .json in /original/
-// check files have correct name in /moved/
